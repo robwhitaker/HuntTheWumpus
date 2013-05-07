3d83a9c95a0720849050c5da59dcad610eb01c44
robwhitaker <Robert.Whitaker1@marist.edu>
2013-05-07 19:53:30 -0400

    Added functionality and fine-tuned the game.  Completely playable.
    - initPrompt handles initial game start input and validates it
    - start takes the validated results of initPrompt and uses them to build the map before passing the State to the gameLoop
    - gameLoop: commented out state command which should not be used in game
    - handleShoot function
        * outputs winningText if the player successfully shoots the Wumpus
        * moves the wumpus (moveWumpus function) and passes the new state to the eventHandler if the player shoots and misses the Wumpus
        * prevents the player from shooting in a direction where there is no passage
    - moveWumpus: randomly picks a room adjacent to the Wumpus (that the Wumpus has passage to) and returns a Wumpus data type with the new location
    - eventHandler now outputs appropriate gameover text if the player runs out of arrows
    - initGame: added one more level of difficulty (3)
    - added a lot of description to the GAMEPLAY TEXT section

--- Main.hs
+++ Main.hs
@@ -5,17 +5,31 @@ module Main (main) where
 	import Player
 	import Wumpus
 	import State
-	import Utility(randBetween,toRoom)
+	import Utility
 	import Data.List(nub)
 	import System.IO
 
 	main :: IO ()
-	main = do
-			putStr "Enter a number of rooms and a difficulty (0-2) separated by a space> "
+	main = initPrompt
+
+	initPrompt :: IO ()
+	initPrompt = 
+		do
+			putStr "Enter a number of rooms (at least 2) and a difficulty between 0 (easiest) and 3 (suicidal) separated by a space: "
 			hFlush stdout
 			initData <- fmap words getLine
-			let numRooms   = ((read . head) initData :: Int)
-			let	difficulty = ((read . last) initData :: Int)
+			if (null initData || (length initData /= 2) || not ((isNumericString $ head initData) && (isNumericString $ last initData)))
+		   		then do {putStrLn "Invalid input!";initPrompt} 
+		   		else do 
+		   				let difficulty = ((read . last) initData :: Int)
+		   				let numRooms   = ((read . head) initData :: Int)
+		   				if (difficulty < 0 || difficulty > 3 || numRooms < 2) 
+		   					then do {putStrLn "Error - Requirements:\n    Number of Rooms > 1\n    0 <= Difficulty <= 3";initPrompt}
+		   					else (start numRooms difficulty)
+
+	start :: Int -> Int -> IO ()
+	start numRooms difficulty =
+		do
 			let gameState  = initGame numRooms difficulty 
 			putStrLn "\nBuilding map..."
 			if (numRooms == (getNumRooms $ gMap gameState)) then (putStrLn "Map successfully generated... \nBeginning game...\n") else putStrLn ""
@@ -29,26 +43,37 @@ module Main (main) where
 					hFlush stdout
 					input <- getLine
 					case input of
-						"state" 					 -> do {putStrLn $ show gameState; gameLoop gameState}
+						--"state" 					 -> do {putStrLn $ show gameState; gameLoop gameState} --only for debugging
 						"help"  					 -> do {putStrLn showHelp; gameLoop gameState}
 						'm':'o':'v':'e':' ':_:[] 	 -> handleMove gameState $ (head . last . words) input
-						's':'h':'o':'o':'t':' ':_:[] -> do {putStrLn input; gameLoop gameState}
+						's':'h':'o':'o':'t':' ':_:[] -> handleShoot gameState $ (head . last . words) input
 						"exit"  					 -> putStrLn "Game exited."
 						_      						 -> do {putStr ("#Error: Invalid command - " ++ input); gameLoop gameState}
 
 	handleMove :: State -> Char -> IO ()
 	handleMove gameState dir 
-		| (snd $ move (gPlayer gameState) (gMap gameState) dir) = do {putStr ("You walk into the next room. "); eventHandler newState}
+		| (snd $ move (gPlayer gameState) (gMap gameState) dir) = do {putStr ("You walk " ++ charDirToStr dir ++ " into the next room. "); eventHandler newState}
 		| otherwise			   = do 
 									putStr "As you stumble in the dark, you carelessly bump into a wall. Perhaps you should try a different direction. " 
 									gameLoop newState
 		where 
 			newState = state (gMap gameState) (fst $ move (gPlayer gameState) (gMap gameState) dir) (gWumpus gameState)
 
+	handleShoot :: State -> Char -> IO ()
+	handleShoot gameState dir
+		| (snd $ fst $ shoot gameState dir) = putStrLn $ winningText $ arrows $ fst $ fst $ shoot gameState dir
+		| (snd $ shoot gameState dir) = do 
+										  putStr $ "You shoot an arrow " ++ charDirToStr dir ++ " into the next room. " ++ wumpusMoveText
+										  eventHandler $ state (gMap gameState) (fst $ fst $ shoot gameState dir) (moveWumpus gameState) 
+		| otherwise					  = do
+										  putStr "It would be a waste of an arrow to shoot at the wall. "
+										  gameLoop gameState
+
 	eventHandler :: State -> IO ()
 	eventHandler gameState
-		| (pLocation $ gPlayer gameState) == (wLocation $ gWumpus gameState) = putStrLn "You barely have a moment to notice that the room reeks of decay when you feel the sharp teeth of the Wumpus dig into your flesh. \n\nGAME OVER.\n"
-		| 0 `elem` (contents $ getRoomById (pLocation $ gPlayer gameState) (gMap gameState)) = putStrLn "As you step forward, you realize a moment too late that the ground has dropped off into a deep chasm.  Your foot slips, and you tumble into the pit.\n\nGAME OVER.\n"
+		| (arrows $ gPlayer gameState) < 1 = putStrLn noArrowsGameOver
+		| (pLocation $ gPlayer gameState) == (wLocation $ gWumpus gameState) = putStrLn wumpusGameOver
+		| 0 `elem` (contents $ getRoomById (pLocation $ gPlayer gameState) (gMap gameState)) = putStrLn pitGameOver
 		| 1 `elem` (contents $ getRoomById (pLocation $ gPlayer gameState) (gMap gameState)) = do 
 																								 putStr batDescription
 																								 eventHandler batRelocationState
@@ -63,8 +88,8 @@ module Main (main) where
 	initGame :: Int -> Int -> State
 	initGame nRooms diff = state newMap (player playerArrows playerLocation) (wumpus wumpusLocation)
 		where
-			newMap = generateMap nRooms
-			playerArrows = (!!) [5,3,1] diff
+			newMap = generateMap nRooms diff
+			playerArrows = (!!) [8,5,3,1] diff
 			playerSafeRooms = filter ((<1) . length . contents) $ getRoomsAsList newMap
 			playerLocation = r_id $ (!!) playerSafeRooms (randBetween (0, length playerSafeRooms - 1))
 			wumpusLocation = r_id $ (!!) (filter ((/=playerLocation) . r_id) $ getRoomsAsList newMap) (randBetween (0,getNumRooms newMap - 2))
@@ -119,20 +144,45 @@ module Main (main) where
 						  | wLocation w == (r_id $ toRoom $ getRoomAtIndex iii m) = ( (player (arrows p - 1) (pLocation p), True), True)
 						  | otherwise = ( (player (arrows p - 1) (pLocation p), False), True)
 
+	moveWumpus :: State -> Wumpus
+	moveWumpus gameState = 
+		case ((!!) wPassage $ randBetween (0,length wPassage - 1)) of
+			'n' -> wumpus $ r_id $ toRoom $ getRoomAtIndex (fst currentRoomIndex - 1, snd currentRoomIndex) (gMap gameState)
+			's' -> wumpus $ r_id $ toRoom $ getRoomAtIndex (fst currentRoomIndex + 1, snd currentRoomIndex) (gMap gameState)
+			'e' -> wumpus $ r_id $ toRoom $ getRoomAtIndex (fst currentRoomIndex, snd currentRoomIndex + 1) (gMap gameState)
+			'w' -> wumpus $ r_id $ toRoom $ getRoomAtIndex (fst currentRoomIndex , snd currentRoomIndex - 1) (gMap gameState)
+		where
+			wPassage = getPassAsStr $ getRoomById (wLocation $ gWumpus gameState) (gMap gameState)
+			currentRoomIndex = getRoomIndex (wLocation $ gWumpus gameState) (gMap gameState)
+
 	-- GAMEPLAY TEXT --
 
 	showHelp :: String
-	showHelp = "================================\n"
-			++ "Hunt The Wumpus HELP MENU\n"
-			++ "================================\n\n"
-			++ "Gameplay Commands\n"
-			++ "-----------------\n"
-			++ "  move [n/e/s/w]\n"
-			++ "  shoot [n/e/s/w]\n\n"
-			++ "Other Commands\n"
-			++ "--------------\n"
-			++ "  help\n"
-			++ "  exit"
+	showHelp = "#================================================================\n"
+			++ "# Hunt The Wumpus HELP MENU\n"
+			++ "#================================================================\n"
+			++ "#\n"
+			++ "# Gameplay Commands\n"
+			++ "#-----------------\n"
+			++ "#     move [n/e/s/w]\n"
+			++ "#     shoot [n/e/s/w]\n"
+			++ "#\n"
+			++ "# Other Commands\n"
+			++ "#--------------\n"
+			++ "#     help\n"
+			++ "#     exit\n"
+			++ "#\n"
+			++ "#================================================================\n"
+			++ "# Gameplay Tips\n"
+			++ "#================================================================\n"
+			++ "#\n"
+			++ "#    * Bottomless Pits emit a soft breeze. Watch your step.\n"
+			++ "#\n"
+			++ "#    * Super Bats are loud while flying and instinctively move\n"
+			++ "#      things around the cave. Don't get grabbed.\n"
+			++ "#\n"
+			++ "#    * The Wumpus has a pungent stench. Use this to track it down\n"
+			++ "#      and kill it."
 
 	initialDescription :: State -> String
 	initialDescription gameState =
@@ -151,7 +201,40 @@ module Main (main) where
 						 "the caverns."
 
 	batDescription :: String
-	batDescription = "A horrible screeching rips through the cavern, forcing you to your knees as you clench your ears. " ++
-			 	 	 "You suddenly feel large claws dig into your shoulders and tear you off the ground. Looking up, you can make out the silhouette " ++
-			 	 	 "of a monstrous bat. Air rushes by your face as your struggle to free yourself, and finally the bat loses its grip, " ++
-			 	 	 "sending you clattering into the ground. After a moment, you manage to get to your feet, only to realize you have no idea where you are. "
+	batDescription = "A horrible screeching rips through the cavern, and " ++
+			 	 	 "a large set of claws suddenly tears you off the ground. You barely make out the silhouette " ++
+			 	 	 "of a monstrous bat before it hurls you " ++
+			 	 	 "into the ground. After a moment, you manage to get to your feet, only to realize you have no idea where you are. "
+
+	winningText :: Int -> String
+	winningText arrs = "\nYou watch the arrow fly down the passage and vanish into the darkness. The cave shakes as an ear shattering roar bursts out of the " ++
+				  "cavern. You hear claws grinding against the stone of the passage as the enraged beast rushes in your direction. You step back and reach " ++
+				  "for another arrow" ++ hasArrowsText
+				  where
+				  		hasArrowsText
+				  			| arrs <= 0 = ", only to realize that you have none left. You turn to run, but the Wumpus crashes into you, pinning you " ++
+				  						  "to the ground under its massive weight. Clenching your eyes, you prepare for its claws to dig into your " ++
+				  						  "abdomin. You think of your family, how you will never see them again.  You think of your village, the people "++ 
+				  						  "that were counting on you. The people that you have failed. And then nothing. You slowly open your eyes and see " ++
+				  						  "the Wumpus's lifeless black eye staring back into yours. Its other eye has been replaced by the shaft of your " ++
+				  						  "arrow. \n\nThe Wumpus is dead! "
+				  			| otherwise = ". The beast leaps into the air as you release the bow string, and the arrow embeds itself " ++
+				  						  "deep within the monster's skull. The creature's body goes limp as it crashes haphazardly into the ground. " ++
+				  						  "\n\nThe Wumpus is dead! "
+
+	wumpusGameOver :: String
+	wumpusGameOver = "You barely have a moment to notice that the room reeks of decay when you feel the sharp teeth of the Wumpus dig into your flesh. " ++
+					 "\n\nGAME OVER.\n"
+
+	pitGameOver :: String
+	pitGameOver = "As you step forward, you realize a moment too late that the ground has dropped off into a deep chasm.  Your foot slips, and you tumble into the pit." ++ 
+				  "\n\nGAME OVER.\n"
+
+	wumpusMoveText :: String
+	wumpusMoveText = "It clatters against the wall, and you suddenly hear the panicked screech of claws " ++ 
+					 "against stone. "
+
+	noArrowsGameOver :: String
+	noArrowsGameOver = "As you the sound rushes in your direction, you reach for a new arrow. Your heart sinks into your " ++
+					   "stomach as you realize the quiver is empty. With a gurgling roar, the Wumpus materialized out of the darkness and " ++
+					   "digs its claws into your stomach. Your vision fades.\n\nGAME OVER.\n"
